b213b83ecdb5a53c9f96323a1e3890e14dcb899c
Maggie McDowell <maggiemcdowell4@gmail.com>
2024-05-25 14:19:59 -0600

    fast project, didnt need a lot of pushes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,10 +16,61 @@ module.exports = () => {
     },
 
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'text editor app',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url:'/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      })
+   
 
-    /*
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
+
+ /*
       TODO:
-
       We need to configure some plugins for Wepack to use. They 
       have already been required in above.
 
@@ -67,12 +118,10 @@ module.exports = () => {
     */
 
 
-    ],
 
-    module: {
-      rules: [
 
-        /*
+
+  /*
           TODO: In this array, we need to configure the rules 
           for when Webpack should "bundle-ize" our files. Each rule 
           is an object. We will be setting up two rules. 
@@ -83,7 +132,4 @@ module.exports = () => {
           When you copy and paste them below, you'll be done here.
         */
 
-      ],
-    },
-  };
-};
+
